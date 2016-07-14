5e24316c5edce34a4ca1ea406e297caecf1f8ab7
someJ <justin_wignall@hotmail.com>
2016-07-14 16:40:13 +0100

    North Europe location

--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -2,5 +2,5 @@
 
 RESOURCE_GROUP=$1
 
-azure group create $1 "eastasia"
+azure group create $1 "northeurope"
 azure group deployment create -f mainTemplate.json -e mainTemplateParameters.json $RESOURCE_GROUP dse
